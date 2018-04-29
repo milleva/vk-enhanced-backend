e91c003a656b76b3ea0091d1905fe50d1a72a1ff
milleva <emiller.arkko@gmail.com>
2018-04-29 16:39:26 +0300

    true dotenv this time

--- controllers/MessageRouter.js
+++ controllers/MessageRouter.js
@@ -5,15 +5,18 @@ const MessageRouter = require('express').Router()
 const nodemailer = require('nodemailer')
 const mg = require('nodemailer-mailgun-transport');
 const StrBuilder = require('../logic/StrBuilder')
+require('dotenv').config()
 
 const auth = {
     auth: {
-        api_key: 'key-92172ded90c90e6a84f7d71624951cc2',
+        api_key: process.env.MAILGUN_API_KEY,
         domain: 'sandbox647cfb0da0144fda92356c27cd535b78.mailgun.org'
     }
 }
 
 const transporter = nodemailer.createTransport(mg(auth))
+
+
 
 MessageRouter.get('/', (req, res) => {
     res.json({activeEmailAddress: "vk@vkry.info"})
